870416dbc39a779bc4384668da00829bbb039c63
Eric Rochester <erochest@virginia.edu>
2013-05-10 15:35:37 -0400

    Added a motor function.

--- src/AtomIno.hs
+++ src/AtomIno.hs
@@ -8,6 +8,9 @@ import System.Directory
 ledPin :: Word8
 ledPin = 13
 
+motorPin :: Word8
+motorPin = 9
+
 high :: Word8
 high = 255
 
@@ -36,9 +39,26 @@ blink = do
 
         where ph = 80000
 
+motor :: Atom ()
+motor = do
+        pin        <- word8 "pin"        motorPin
+        level_low  <- word8 "level_low"  low
+        level_high <- word8 "level_high" high
+        let writePin = digitalWrite (vue pin)
+
+        period ph $ phase 0 $ atom "motor" $ do
+            phase 0 $ atom "motor_on" $
+                writePin $ vue level_high
+            phase onTime $ atom "motor_off" $
+                writePin $ vue level_low
+
+        where onTime  = 2500
+              offTime = 100000
+              ph      = onTime + offTime
+
 main :: IO ()
 main = do
-    (sch, _, _, _, _) <- compile atomName defaults { cCode = prePostCode } blink
+    (sch, _, _, _, _) <- compile atomName defaults { cCode = prePostCode } motor
     putStrLn $ reportSchedule sch
     renameFile (atomName ++ ".c") (atomName ++ ".pde")
     where
